c7ad647a49ec99b18a227a6a5218c4dd6abcf125
Andrew <kolbe1129@gmail.com>
2020-10-14 17:24:24 -0400

    Completed with Review

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const friends = ['Tucker', 'Brent', 'Brodie', 'Hunter', 'Chad'];
+const friends = ['Chad', 'Chadd', 'Chaz', 'Chaddington', 'Chaddy'];
 
 //create button to activate song
 let button = document.createElement('button');
@@ -8,33 +8,39 @@ document.body.appendChild(button);
 
 //looping through friends
 function friendLoop() {
-    for (let i = 0; i < friends.length; i++) {
+    for (const friend of friends) {
+
         let div = document.createElement('div');
         div.className = 'friend';
 
         let h3 = document.createElement('h3');
-        let h3text = document.createTextNode(friends[i]);
+        let h3text = document.createTextNode(friend);
         h3.appendChild(h3text);
         div.appendChild(h3);
 
 
-        let p = document.createElement('p');
-        let pText;
-        for (let i = 0; i < friends.length; i++) {
-            for (let j = 99; j > 0; j--) {
-                if (j > 1) {
-                    pText = document.createTextNode(j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file');
-                } else {
-                    (j + ' line of code in the file, ' + j + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
-                }
+        for (let j = 99; j > 0; j--) {
+            let p = document.createElement('p');
+            let pText;
+
+            //if should catch the smallest amount possible //Leave the majority to else
+            if (j === 1) {
+                pText = document.createTextNode(j + ' line of code in the file, ' + j + ' line of code; ' + friend + ' strikes one out, clears it all out, no more lines of code in the file');
+            } else if (j === 2) {
+                pText = document.createTextNode(j + ' lines of code in the file, ' + j + ' lines of code; ' + friend + ' strikes one out, clears it all out, ' + (j - 1) + ' line of code in the file');
+            } else {
+                pText = document.createTextNode(j + ' lines of code in the file, ' + j + ' lines of code; ' + friend + ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file');
             }
+        
+            p.appendChild(pText);
+            div.appendChild(p);
+            document.body.appendChild(div);
         }
-
-        p.appendChild(pText);
-        div.appendChild(p);
-
-        document.body.appendChild(div);
     }
+
+   
+   
 }
+
 //combining friend loop with click
 button.addEventListener('click', friendLoop);
